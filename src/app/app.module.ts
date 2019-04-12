91e5485f6275771e3e729f18186adb30176f40e4
szuperaz <szuperaz@gmail.com>
2019-04-12 15:44:02 +0200

    heart pipe added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HoverDirective } from './hover.directive';
+import { HeartPipe } from './heart.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HoverDirective
+    HoverDirective,
+    HeartPipe
   ],
   imports: [
     BrowserModule
